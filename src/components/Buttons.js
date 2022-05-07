f43e7e2b284b37c915b73b975608624663a8fcce
Hain Thit <hainlthit@gmail.com>
2022-05-07 18:28:46 -0400

    dark mode

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,91 @@
 import styled from "styled-components";
 import { defaultTheme, typeScale } from "../utils";
+import { applyStyleModifiers } from "styled-components-modifiers";
+
+const BUTTON_MODIFIERS = {
+  small: () => `
+    font-size: ${typeScale.helperText};
+    padding: 8px;
+  `,
+  large: () => `
+    font-size: ${typeScale.h5};
+    padding: 16px 24px;
+  `,
+  warning: () => `
+  background-color: ${defaultTheme.status.warningColor};
+  color: ${defaultTheme.textColor};
+
+  &:hover, &:focus {
+    background-color: ${defaultTheme.status.warningColorHover};
+    outline: 3px solid ${defaultTheme.status.warningColorHover};
+  }
+
+  &:active {
+    background-color: ${defaultTheme.status.warningColorActive}
+  }
+  `,
+  secondaryButtonWarning: () => `
+  background: none;
+  border: 2px solid ${defaultTheme.status.warningColor};
+  color: ${defaultTheme.status.warningColor};
+  `,
+  tertiaryButtonWarning: () => `
+  background: none;
+  color: ${defaultTheme.status.warningColor};
+  `,
+
+  error: () => `
+  background: none;
+  color: ${defaultTheme.status.errorColor};
+  &:hover, &:focus {
+    background-color: ${defaultTheme.status.errorColorHover};
+    outline: 3px solid ${defaultTheme.status.errorColorHover};
+    outline-offset: 2px;
+    border: 2px solid transparent;
+  }
+  &:active {
+    background-color: ${defaultTheme.status.errorColorActive};
+  }
+`,
+  primaryButtonError: () => `
+    background-color: ${defaultTheme.status.errorColor};
+    color: ${defaultTheme.textColorInverted};
+`,
+  secondaryButtonError: () => `
+    background: none;
+    border: 2px solid ${defaultTheme.status.errorColor};
+    color: ${defaultTheme.status.errorColor};
+`,
+  tertiaryButtonError: () => `
+  background: none;
+  color: ${defaultTheme.status.errorColor};
+  `,
+
+  success: () => `
+  background: none;
+  color: ${defaultTheme.status.successColor};
+  &:hover, &:focus {
+    background-color: ${defaultTheme.status.successColorHover};
+    outline: 3px solid ${defaultTheme.status.successColorHover};
+    outline-offset: 2px;
+    border: 2px solid transparent;
+  }
+  &:active {
+    background-color: ${defaultTheme.status.successColorActive};
+  }
+`,
+primaryButtonSuccess: () => `
+  background-color: ${defaultTheme.status.successColor};
+  color: ${defaultTheme.textColorInverted};
+`,
+secondaryButtonSuccess: () => `
+  border: 2px solid ${defaultTheme.status.warningColor};
+`,
+tertiaryButtonSuccess: () => `
+  background: none;
+
+`
+};
 
 const Button = styled.button`
   padding: 12px 24px;
@@ -31,18 +117,43 @@ const PrimaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 export const SecondaryButton = styled(Button)`
   background: none;
   border: 2px solid ${defaultTheme.primaryColor};
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background-color: none;
+    border-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 export const TertiaryButton = styled(Button)`
   background: none;
   border: none;
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
+
+  ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ import { GlobalStyle } from "./utils"
 
 const App = () => (
   <div>
+    <div>
     <PrimaryButton> Hello World</PrimaryButton>
     <SecondaryButton> Goodbye World</SecondaryButton>
     <TertiaryButton> Hallo World</TertiaryButton>
+    </div>
+    <div>
+    <PrimaryButton disabled> Hello World</PrimaryButton>
+    <SecondaryButton disabled> Goodbye World</SecondaryButton>
+    <TertiaryButton disabled> Hallo World</TertiaryButton>
+    </div>
+    <div>
+    <PrimaryButton modifiers={["small", "error"]}> Hello World</PrimaryButton>
+    <SecondaryButton modifiers={["large", "warning", "secondaryButtonWarning"]}> Goodbye World</SecondaryButton>
+    <TertiaryButton modifiers={["success", "tertiaryButtonSucces"]}s> Hallo World</TertiaryButton>
+    </div>
     <GlobalStyle />
   </div>
 );
